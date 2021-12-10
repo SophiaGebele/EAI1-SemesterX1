5dd5eddb9bf7222e433fce6fec31731471332505
SophiaGebele <gebele.sophia@web.de>
2021-12-10 16:06:36 +0100

    9

---
+++ Aufgabe 9/Script9.js
@@ -0,0 +1,8 @@
+window.addEventListener("load", function bla() {
+    document.getElementById("alleloschen").addEventListener("click", function () {
+    });
+});
+//bei eingabe und danach keydown auf return soll der eingegebene
+//text soll der klasse erledigungen hinzugefuegt werden
+//der buttin alleloeschen entfernt den text aus der klasse
+//# sourceMappingURL=Script9.js.map
---
+++ Aufgabe 9/Script9.ts
@@ -0,0 +1,11 @@
+window.addEventListener("load", function bla(): void {
+   document.getElementById("alleloschen").addEventListener("click", function(){
+       
+    })
+
+    
+})
+
+//bei eingabe und danach keydown auf return soll der eingegebene
+//text soll der klasse erledigungen hinzugefuegt werden
+//der buttin alleloeschen entfernt den text aus der klasse
